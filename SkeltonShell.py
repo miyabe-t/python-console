2fef4fc6e6730969c4d211f4b2c6d4be0e79f199
miyabet <orisiso.ebaim@gmail.com>
2018-06-09 11:10:35 +0900

    Code refactoring, creation of sample script
    Changed scripts to obey the pep8 code style.
    Sample script was created.

--- SkeltonShell.py
+++ SkeltonShell.py
@@ -2,25 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import shlex
-from modules.ShellModules import ShellModules
 from modules._getch import _Getch
 
-VERSION='0.1'
+VERSION = '0.1'
 
+
+# TODO detect current cursor pos, delete chars must follow that pos
 class ShellInputter:
     def __init__(self):
         self.size = [0, 0]
         self.history = ['']
         self.hist_cur = len(self.history)
+        self.cursor = 0
         self.binds = []
         self.prompt = '>>> '
         self.getch = _Getch()
-        # regist key bindings to itself
+        # regist special keys (console specific keys)
         self.regist(chr(0x03), self.clear)
-        self.regist(chr(0x08), self.backspace)
-        self.regist(chr(0x7f), self.backspace)
+        self.regist('\x15', self.delete_all)      # C-u
+        self.regist('\x17', self.delete_word)     # C-w
+        self.regist(chr(0x08), self.delete_char)  # C-h
+        self.regist(chr(0x7f), self.delete_char)
         self.regist(chr(0x10), self.historyUp)
         self.regist('\x1b[A', self.historyUp)
         self.regist(chr(0x0e), self.historyDown)
@@ -35,22 +38,21 @@ class ShellInputter:
         self.prompt = prmpt
 
     def input(self):
-        import sys
         cmd = ''
         term = False
-        cur = len(self.history)
         print(self.prompt),
         while True:
             ch = self.getch()
             matched = False
             for bind in self.binds:
-                if ch == bind['key'] :
+                if ch == bind['key']:
                     cmd, term = bind['callback'](cmd)
                     matched = True
             if not matched:
                 cmd += ch
-            if term : break
-            print('\r' + ' '*(self.size[1] - 1) ),
+            if term:
+                break
+            print('\r' + ' '*(self.size[1] - 1)),
             print('\r' + self.prompt + cmd),
         print('')
         self.history.append(cmd.strip())
@@ -60,18 +62,35 @@ class ShellInputter:
 
     def clear(self, cmd):
         return '', True
-    def backspace(self, cmd):
+
+    def delete_all(self, cmd):
+        return '', False
+
+    def delete_char(self, cmd):
         return cmd[:-1], False
+
+    def delete_word(self, cmd):
+        words = cmd.split(' ')
+        return ' '.join(words[:-1]), False
+
     def newline(self, cmd):
         return cmd, True
+
     def historyUp(self, cmd):
-        self.hist_cur = self.hist_cur-1 if self.hist_cur > 0 else self.hist_cur
+        if self.hist_cur > 0:
+            self.hist_cur = self.hist_cur-1
         return self.history[self.hist_cur], False
+
     def historyDown(self, cmd):
-        self.hist_cur = self.hist_cur+1 if self.hist_cur < len(self.history)-1 else len(self.history)-1
+        if self.hist_cur < len(self.history)-1:
+            self.hist_cur = self.hist_cur+1
+        else:
+            len(self.history)-1
         return self.history[self.hist_cur], False
+
     def regist(self, key, callback):
-        self.binds.append({ 'key':key, 'callback':callback } )
+        self.binds.append({'key': key, 'callback': callback})
+
 
 class ShellEvaluator:
     def __init__(self, modules):
@@ -80,40 +99,42 @@ class ShellEvaluator:
         self.suffix = '_aliases'
         self.core = modules
 
-        # search functions start with `self.prefix` and convert them
-        # into a dictionary which contains a commands list a and callback method
-        # via `self.regist()`. Aliases are also defined using nameing rule.
-        func_names = [ func for func in dir(self.core) if 'cbk_' in func ]
+        # search functions start with `self.prefix` and convert them into a
+        # dictionary which contains a commands list a and callback method via
+        # `self.regist()`. Aliases are also defined using nameing rule.
+        func_names = [func for func in dir(self.core) if 'cbk_' in func]
         for func_name in func_names:
             func = getattr(self.core, func_name)
-            cmd = [ func_name, func_name.replace( self.prefix, '' ) ]
+            cmd = [func_name, func_name.replace(self.prefix, '')]
             if hasattr(self.core, cmd[1] + self.suffix):
                 for item in getattr(self.core, cmd[1]+self.suffix):
                     cmd.append(item)
-            self.regist( cmd , func)
+            self.regist(cmd, func)
 
     def regist(self, commands, callback):
         '''
-        Parameters
-        -----------
-        commands : list of string
-            a command you want to use with aliases of that command included in one list
-        callback : function
+        Args:
+        commands (list(str)):
+            a command you want to use with aliases of that command included in
+            one list
+        callback (function):
             this fucntion will be called for `commands`
         '''
-        self.cmds.append( { 'name':commands, 'callback':callback } )
+        self.cmds.append({'name': commands, 'callback': callback})
 
     def help(self):
         global global_help
         print(global_help)
 
     def evaluate(self, args):
-        if len(args) == 0 : return
+        if len(args) == 0:
+            return
         for cmd in self.cmds:
-            if args[0] in cmd['name'] :
+            if args[0] in cmd['name']:
                 return cmd['callback'](args[1:])
-        else :
+        else:
             print('No such a command : {0}'.format(args[0]))
+
 
 class ShellOutputter:
     def __init__(self):
@@ -126,8 +147,9 @@ class ShellOutputter:
     def output(self):
         pass
 
-class ShellSkelton:
-    def __init__(self, modules=ShellModules() ):
+
+class SkeltonShell:
+    def __init__(self, modules):
         self.inputter = ShellInputter()
         self.evaluator = ShellEvaluator(modules)
         self.outputter = ShellOutputter()
@@ -138,8 +160,8 @@ class ShellSkelton:
     def checkSize(self):
         self.rows, self.columns = os.popen('stty size', 'r').read().split()
         self.rows, self.columns = int(self.rows), int(self.columns)
-        self.inputter.setConsoleSize( self.rows, self.columns )
-        self.outputter.setConsoleSize( self.rows, self.columns )
+        self.inputter.setConsoleSize(self.rows, self.columns)
+        self.outputter.setConsoleSize(self.rows, self.columns)
 
     def main(self):
         '''This function is the main loop of interactive shell.
@@ -149,8 +171,10 @@ class ShellSkelton:
         while True:
             cmd = self.inputter.input()
             ret = self.evaluator.evaluate(cmd)
-            if ret == -1: break
+            if ret == -1:
+                break
             self.outputter.output()
-            
+
+
 if __name__ == '__main__':
     print('Running this file directly has no meaning...')
---
+++ example.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Example use of SkeltonShell
+===========================
+
+This file describes how to use SkeltonShell.
+The Class named `ModuleExample` is just example
+of a module which is passed to SkelltonShell.
+
+A module must have commands, aliases of commands and
+function invoked by commands. You must follow the naming
+convention described in README.
+"""
+from SkeltonShell import SkeltonShell
+
+
+class ModuleExample:
+    def __init__(self):
+        self.test_aliases = ['t']
+        self.quit_aliases = ['q', 'exit']
+
+    def cbk_test(self, args):
+        print('This is the funtion named "cbk_test"')
+        print('Here goes your arguments list : '),
+        print(args)
+
+    def cbk_quit(self, args):
+        return -1
+
+
+"""
+Entry point of this sample script. Note that SkeltonShell's argument
+must be instance of the calss, not class itself. Then just call main()
+to begin main loop of the program, interactive shell.
+"""
+if __name__ == '__main__':
+    ex = SkeltonShell(modules=ModuleExample())
+    ex.main()
